cc9df4e1b5e74f372cd4aea1fb4bb55d09383fab
rin-kinokocan <rin1702-se@kinoko-can.com>
2020-11-16 01:48:35 +0900

    Color for terminals cannot change color.

--- class/Color.hpp
+++ class/Color.hpp
@@ -17,10 +17,18 @@ namespace scgb{
     }
     
     attr_t GetColorPair(int r,int g,int b){
+      if(!has_colors())
+	return 0;
       int res=0,max=0;
-      r=r*1000/255.0;
-      g=g*1000/255.0;
-      b=b*1000/255.0;
+      int ColMax;
+      if(can_change_color)
+	ColMax=1000;
+      else
+	ColMax=680;
+      
+      r=r*ColMax/255.0;
+      g=g*ColMax/255.0;
+      b=b*ColMax/255.0;
       for(int i=1;i<COLOR_PAIRS;i++){
 	short pf,pb,cr,cg,cb;
 	pair_content(i,&pf,&pb);
@@ -39,12 +47,17 @@ namespace scgb{
     short GetColor(int r,int g,int b){
       //rgb values:0~255
       if(!has_colors())
-	return 0;
+	return 0;      
       short cr,cg,cb;
       int res=0,max=0;
-      r=r*1000/255.0;
-      g=g*1000/255.0;
-      b=b*1000/255.0;
+      int ColMax;
+      if(can_change_color)
+	ColMax=1000;
+      else
+	ColMax=680;
+      r=r*ColMax/255.0;
+      g=g*ColMax/255.0;
+      b=b*ColMax/255.0;
       for(int i=0;i<COLORS;i++){
 	color_content(i,&cr,&cg,&cb);
 	int dist=pow(r-cr,2)+pow(g-cg,2)+pow(b-cb,2);
